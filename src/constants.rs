dd90dd0b42238ad2f6c5eedae524bba4c4b018b7
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2024-01-31 16:09:57 +0700

    Init project
    - Basic webhook
    - Basic dialogue

---
+++ src/constants.rs
@@ -0,0 +1,25 @@
+#![allow(non_camel_case_types)]
+
+use std::env;
+pub enum EEnvKey {
+    BOT_TOKEN,
+    NGROK_AUTHTOKEN,
+    WEBHOOK_DOMAIN,
+    SOCKET_ADDR,
+}
+
+impl EEnvKey {
+    fn to_str(&self) -> &str {
+        match self {
+            EEnvKey::BOT_TOKEN => "BOT_TOKEN",
+            EEnvKey::NGROK_AUTHTOKEN => "NGROK_AUTHTOKEN",
+            EEnvKey::WEBHOOK_DOMAIN => "WEBHOOK_DOMAIN",
+            EEnvKey::SOCKET_ADDR => "SOCKET_ADDR",
+        }
+    }
+
+    pub fn get_env_value(&self) -> String {
+        let key = self.to_str();
+        env::var(key).expect(format!("{} must be set!", key).as_str())
+    }
+}
